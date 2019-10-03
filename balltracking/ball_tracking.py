a238954228f58acf3763355b40e6a345f7a5afb1
blissdismissed <ajautz@gmail.com>
2019-10-03 11:59:27 -0400

    creating a chemtrail of the path, haha

--- balltracking/ball_tracking.py
+++ balltracking/ball_tracking.py
@@ -53,3 +53,59 @@ while True:
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
+    
+    # find contours in mask, init center
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    
+    # only process if at least one contour is found
+    if len(cnts) > 0:
+        # find largest contour in mask
+        # compute min enclosing circle and centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x,y), radius) = cv2.minEnclosingCircle(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        
+        # only proceed if radius meets a min size
+        if radius > 10:
+            # draw circle and centroid on frame
+            # update list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            
+    # update the points queue
+    pts.appendleft(center)
+    
+    # loop over set of tracked points
+    for i in range(1, len(pts)):
+        # if either are None, ignore
+        if pts[i-1] is None or pts[i] is None:
+            continue
+        
+        # otherwise draw it
+        thickness = int(np.sqrt(Args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i-1], pts[i], (0, 0, 255), thickness)
+        
+    # show frame to screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    
+    # if q is pressed stop the loop
+    if key == ord("q"):
+        break
+
+# if not using a video file, stop the camera stream
+if not args.get("video", False):
+    vs.stop()
+
+# otherwise, release the camera
+else:
+    vs.release()
+    
+# close all windows
+cv2.destroyAllWindows()
+
+    
+    
